a6fd2600567620f179f9a64b0e5328b64c8f112e
Marcos Alvarez <31220059+alvmivan@users.noreply.github.com>
2020-07-05 02:50:48 -0300

    Hand Aim

---
+++ Gamework Game/Assets/Scripts/Utils/GOExtensions.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Utils
+{
+    public static class GOExtensions
+    {
+        public static TComponent GetOrCreate<TComponent>(this GameObject go) where TComponent : Component
+        {
+            var component = go.GetComponent<TComponent>();
+            return component ? component : go.AddComponent<TComponent>();
+        }
+    }
+}
---
+++ Gamework Game/Assets/Scripts/Utils/Maths.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Utils
+{
+    public static class Maths
+    {
+        public static Vector3 Up(this Quaternion rotation)
+        {
+            return new Vector3
+            {
+                x = rotation.x * (rotation.y * 2f) - rotation.w * (rotation.z * 2f),
+                y = 1.0f - (rotation.x * (rotation.x * 2f) + rotation.z * (rotation.z * 2f)),
+                z = rotation.y * (rotation.z * 2f) + rotation.w * (rotation.x * 2f)
+            };
+        }
+
+        public static float Dot(this Vector2 vec, Vector2 other)
+        {
+            return Vector2.Dot(vec, other);
+        }
+        public static float Dot(this Vector3 vec, Vector3 other)
+        {
+            return Vector3.Dot(vec, other);
+        }
+    }
+}
